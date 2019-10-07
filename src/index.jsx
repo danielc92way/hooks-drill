f05165289176bf6ed0cc359cce89d6f9108c53cd
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-08 10:14:02 +1100

    Added name reducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
@@ -5,6 +6,8 @@ import { Provider } from 'react-redux';
 import App from './App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));

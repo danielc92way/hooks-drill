7bcb7078ab2acc43ce78e0db54c9824935b6a36d
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-08 09:41:18 +1100

    Added buttons with dispatch events

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { countUp, countDown, resetCount } from './actions';
 
 export default function App() {
   const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
 
   return (
     <div>
-      {`The count is ${count}`}
+      <p>{`The count is ${count}`}</p>
+      <button type="button" onClick={() => dispatch(countUp)}>Count Up</button>
+      <button type="button" onClick={() => dispatch(countDown)}>Count Down</button>
+      <button type="button" onClick={() => dispatch(resetCount)}>Count Reset</button>
     </div>
   );
 }

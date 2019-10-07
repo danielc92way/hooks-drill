d8abb01e6b3455de67982fbca80ccec17d39d5dd
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-08 09:34:13 +1100

    Filled in reducer

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,3 +1,14 @@
-const rootReducer = (state = { count: 0 }, action) => state;
+const rootReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'COUNT_UP':
+      return { ...state, count: state.count + 1 };
+    case 'COUNT_DOWN':
+      return { ...state, count: state.count - 1 };
+    case 'RESET':
+      return { ...state, count: action.value };
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;

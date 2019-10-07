9c17f90624c0ddbcde66b9c825a98194f3221f4d
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-08 09:22:21 +1100

    Add store

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = (state = { count: 0 }, action) => state;
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

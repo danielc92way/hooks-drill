d08ae12649d38b6367d44e02b5ad85876f327fe7
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-10 13:39:47 +1100

    Seperated out CountContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import Headline from './Headline';
-import NameContainer from './components/NameContainer';
-import { countUp, countDown, resetCount } from './actions';
-import './App.css';
+import CountContainer from './components/CountContainer';
 import TasksContainer from './components/TasksContainer';
+import NameContainer from './components/NameContainer';
+import './App.css';
+
 
 export default function App() {
-  
-  // Retrieving global state
-  const count = useSelector((state) => state.count.count);
-  
-  // Dispatcher methods
-  const dispatch = useDispatch();
-  const dispatchCountUp = () => dispatch(countUp);
-  const dispatchCountDown = () => dispatch(countDown);
-  const dispatchCountReset = () => dispatch(resetCount);
-  
   return (
     <section className="container">
+
       <div className="padder">
         <Headline text="A random headline" />
       </div>
-
       <NameContainer />
-
-      <div className="padder">
-        <h3>
-          The count from global state is&nbsp;
-          <span className="highlight">{count}</span>
-        </h3>
-        <button type="button" onClick={dispatchCountUp}>
-          Count Up
-        </button>
-        <button type="button" onClick={dispatchCountDown}>
-          Count Down
-        </button>
-        <button type="button" onClick={dispatchCountReset}>
-          Count Reset
-        </button>
-      </div>
-
-     <TasksContainer/>
+      <CountContainer />
+      <TasksContainer />
 
     </section>
   );
---
+++ src/components/CountContainer/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { countUp, countDown, resetCount } from '../../actions';
+
+export default function CountContainer() {
+
+    const count = useSelector((state) => state.count.count);
+  
+    const dispatch = useDispatch();
+    const dispatchCountUp = () => dispatch(countUp);
+    const dispatchCountDown = () => dispatch(countDown);
+    const dispatchCountReset = () => dispatch(resetCount);
+    
+    return (
+      <div className="padder">
+        <h3>
+          The count from global state is&nbsp;
+          <span className="highlight">{count}</span>
+        </h3>
+        <button type="button" onClick={dispatchCountUp}>
+          Count Up
+        </button>
+        <button type="button" onClick={dispatchCountDown}>
+          Count Down
+        </button>
+        <button type="button" onClick={dispatchCountReset}>
+          Count Reset
+        </button>
+      </div>
+    )
+}
